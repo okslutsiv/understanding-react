f0262223132ddd5b007c32706b266cda9fa8d1b2
okslutsiv <okslutsiv@gmail.com>
2024-11-14 16:02:06 +0100

    init

--- app.js
+++ app.js
@@ -1,57 +1,52 @@
-const rootNode = document.getElementById("root");
-
+const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
-
-console.log(root);
-
-const sayHello = () => {
-  console.log("running function");
-  return React.createElement("p", null, "Hello from function!");
-};
-
-const hello = React.createElement("p", null, "Hello from const!");
-root.render(React.createElement(App));
-let counterName = "One";
+root.render(<App />);
 
 function App() {
-  return React.createElement(
-    "main",
-    null,
-    React.createElement("h1", null, "Counters"),
-    React.createElement(
-      "section",
-      null,
-      counterName === "One"
-        ? React.createElement(Counter, { name: counterName })
-        : React.createElement(Counter2, { name: counterName })
-    ),
-    React.createElement("button", null, "Click me"),
-    sayHello()
+  return (
+    <>
+      <h1>Counters</h1>
+      <section>
+        <Counter name="One" />
+      </section>
+    </>
   );
 }
 
-function Counter({ name }) {
-  return React.createElement(
-    "article",
-    null,
-    React.createElement("h2", null, `Counter ${name}`),
-    React.createElement("p", null, "You clicked 1 time")
+function Counter(props) {
+  const numOfClicksRef = React.useRef({ total: 0 });
+  const buttonRef = React.useRef();
+  const [count, setCount] = React.useState(0);
+
+  function handleClick() {
+    numOfClicksRef.current.total = numOfClicksRef.current.total + 1;
+    alert(`You've clicked ${numOfClicksRef.current.total} times.`);
+    console.log(root);
+  }
+
+  console.log("running Counter");
+
+  React.useEffect(() => {
+    buttonRef.current.focus();
+  }, []);
+
+  return (
+    <article>
+      <h2>Counter {props.name}</h2>
+      <p>You clicked {numOfClicksRef.current.total} times</p>
+      <p>You setted count to be {count}</p>
+
+      <p>
+        <button className="button" onClick={handleClick} ref={buttonRef}>
+          Click me to change ref current
+        </button>{" "}
+        <button
+          className="button"
+          onClick={() => setCount((count) => count + 2)}
+        >
+          Click me to change state
+        </button>
+      </p>
+    </article>
   );
 }
-
-function Counter2({ name }) {
-  return React.createElement(
-    "article",
-    null,
-    React.createElement("h2", null, `Counter ${name}`),
-    React.createElement("p", null, "Times clicked 2")
-  );
-}
-
-function rerender() {
-  console.log("Rerendering...");
-  counterName = "Two";
-  root.render(React.createElement(App));
-}
-
-console.log(App());
